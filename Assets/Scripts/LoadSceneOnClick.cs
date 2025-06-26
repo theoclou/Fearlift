694c3c14fa8c240a4083e11396e176a65b55fa30
Quentin <quentin.mariat@insa-lyon.fr>
2025-06-26 10:41:30 +0900

    premier init

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    public string sceneName;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
